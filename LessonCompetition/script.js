e38f0fd375a826dfb8e5700fc2b595908f797027
Anastasiya Shkrogal <straddi@mail.ru>
2020-03-01 20:11:49 +0300

    Добавляется первый герой автоматически при выборе фильм и добавлены кнопки для лучшей навигации

--- LessonCompetition/script.js
+++ LessonCompetition/script.js
@@ -12,9 +12,14 @@ document.addEventListener('DOMContentLoaded', () => {
         date = document.createElement('div'),
         actor = document.createElement('div'),
         ulMovieList = document.createElement('ul'),
-        divStatus = document.createElement('div');
+        divStatus = document.createElement('div'),
+        btnLeft = document.createElement('a'),
+        btnRight = document.createElement('a'),
+        divWrapper = document.createElement('div'),
+        style = document.createElement('style');
 
-    let fontSize = '9px';
+    let fontSize = '9px',
+        data = {};
 
     //стилизация элементов
     document.body.style.cssText = `
@@ -73,34 +78,74 @@ document.addEventListener('DOMContentLoaded', () => {
     selectFilm.style.cssText = `
     font-size: ${fontSize};
     `;
+    divWrapper.style.cssText = `
+    width: 30%;
+    display: flex;
+    justify-content: space-between;
+    align-items: flex-start;
+    display: none;
+    `;
 
-
-    //Настройка select перед началом
+    //Настройка select и кнопок  перед началом
     option.value = '0';
     option.innerHTML = 'Выберите героя';
     option.selected = true;
     optionFilm.value = '0';
     optionFilm.innerHTML = 'Выберите фильм';
     optionFilm.selected = true;
+    btnLeft.id = 'left';
+    btnRight.id = 'right';
 
+    // Создание стилей для кнопок при наведении
+    style.textContent = `
+    #left:hover, #right:hover{
+        opacity: 0.8; 
+        background-color: #CCCCCC ;
+        transition: All .2s; 
+    }
+    #right {
+        margin-top: 140px;
+        width: 24px;
+        height: 24px;
+        background-size: 24px;
+        background-image: url("./images/right-arrow.svg");
+        background-color: #DDDEEE ;
+        opacity: 0.4; 
+        border-radius: 10px;
+    }
+    #left {
+        margin-top: 140px;
+        width: 24px;
+        height: 24px;
+        background-size: 24px;
+        background-image: url("./images/left-arrow.svg");
+        background-color: #DDDEEE ;
+        opacity: 0.3; 
+        border-radius: 10px;
+    }
+    `;
     //добавление элементов на страницу
     select.appendChild(option);
     selectFilm.appendChild(optionFilm);
     form.appendChild(selectFilm);
     form.appendChild(select);
-    document.body.append(form);
-    document.body.append(container);
+    divWrapper.appendChild(btnLeft);
+    divWrapper.appendChild(container);
+    divWrapper.appendChild(btnRight);
     container.appendChild(img);
     container.appendChild(info);
     container.appendChild(date);
     container.appendChild(actor);
     container.appendChild(ulMovieList);
     container.appendChild(divStatus);
+    document.body.append(form);
+    document.body.append(divWrapper);
+    document.head.appendChild(style);
 
     //Функция создания options у selectFilm
-    const addOptionFilm = (resp) => {
+    const addOptionFilm = () => {
         const set = new Set();
-        resp.forEach(item => {
+        data.forEach(item => {
             !!item.movies ? item.movies.forEach(val => set.add(val)) : null;
 
         });
@@ -113,14 +158,15 @@ document.addEventListener('DOMContentLoaded', () => {
     };
 
     //Функия создания options у select 
-    const addOption = (selected, resp) => {
+    const addOption = (selected) => {
+        divWrapper.style.display = 'flex';
         //Очищение select перед выбором;
         let { length } = select.options;
         for (let i = 1; i < length; i++) {
             select.remove(select.options[i]);
         };
         // Поиск по фильму
-        resp.forEach(item => {
+        data.forEach(item => {
             if (item.movies && item.movies.some(film => film === selected)) {
                 const optionAdd = document.createElement('option');
                 optionAdd.value = item.name;
@@ -128,13 +174,15 @@ document.addEventListener('DOMContentLoaded', () => {
                 select.appendChild(optionAdd);
             };
         });
-
+        addInfoAfterSelect(select.options[1].value);
+        divWrapper.className = select.options[1].value;
     };
 
     //Функция отображения информации после выбора всех selectов
-    const addInfoAfterSelect = (selected, response) => {
-        response.forEach(item => {
+    const addInfoAfterSelect = (selected) => {
+        data.forEach(item => {
             if (item.name === selected) {
+                divWrapper.className = selected;
                 img.src = item.photo;
                 img.innerHTML = item.name;
                 info.innerHTML = `<strong>Имя:</strong> ${item.name} <br>
@@ -163,24 +211,50 @@ document.addEventListener('DOMContentLoaded', () => {
         request.send();
         request.addEventListener('readystatechange', () => {
             if (request.readyState === 4 && request.status === 200) {
-                const data = JSON.parse(request.responseText);
-                callback(data);
+                data = JSON.parse(request.responseText);
+                callback();
             } else if (request.readyState === 4) {
                 console.warn('Запрос к серверу пришёл с ошибкой');
             };
         });
     };
+    //Смена карточек влево
+    const moveCardsLRight = () => {
+        let index = [...select.options].map(item => item.value).indexOf(divWrapper.className);
+        index++;
+        if (index > ([...select.options].length - 1)) {
+            index = 1;
+        };
+        select.selectedIndex = index;
+        addInfoAfterSelect(select.options[select.selectedIndex].value);
+    };
+    // Смена карточек вправо
+    const moveCardsLeft = () => {
+        let index = [...select.options].map(item => item.value).indexOf(divWrapper.className);
+        index--;
+        if (index === 0) {
+            index = [...select.options].length - 1;
+        };
+        console.log(index);
+        select.selectedIndex = index;
+        addInfoAfterSelect(select.options[select.selectedIndex].value);
+    };
 
     //Запрос на сервер для отображения пунктов меню фильмов
     requestDoing(addOptionFilm);
 
-    //Обработчики событий на selectы
+    // Обработчик событий на кнопки
+    divWrapper.addEventListener('click', (e) => {
+        if (e.target.tagName === 'A') {
+            e.target.id === 'left' ? moveCardsLeft() : moveCardsLRight();
+        }
+    })
 
+    //Обработчики событий на selects
     selectFilm.addEventListener('change', (event) => {
-        requestDoing(addOption.bind(null, event.target.value));
+        addOption(event.target.value);
     });
     select.addEventListener('change', (event) => {
-        requestDoing(addInfoAfterSelect.bind(null, event.target.value));
+        addInfoAfterSelect(event.target.value);;
     });
-
 });
